ac78bc9ed23473e44c7c1e24d2898a1ee66b2745
Naumantamboli <tambolinauman7@gmail.com>
2024-02-27 21:00:36 +0530

    pushed all files

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn1 = document.querySelector('subsection2 .box #one')
+let btn2 = document.querySelector('subsection2 .box #two')
+
+let premium = document.querySelector('#premium')
+let install = document.querySelector('#install')
+
+let play = document.querySelector('#play')
+let follow = document.querySelector('#follow')
